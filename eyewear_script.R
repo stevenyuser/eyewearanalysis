c371ec6b3e8ece20dbd04d791960fd817faaf2c2
Steven Yu <stevenyuser@gmail.com>
2021-06-18 18:28:58 -0400

    added comments

--- eyewear_script.R
+++ eyewear_script.R
@@ -3,7 +3,7 @@
 # load patternize
 library(patternize)
 
-# List with samples
+# List with samples -- reduced list for testing
 IDlist <- c('LabA3',
             'LabB3',
             'ReadingA2',
@@ -20,13 +20,17 @@ extension <- '.png'
 imageList <- makeList(IDlist, 'image', prepath, extension)
 
 # align color patterns
-RGB <- c(207, 90, 0) # red color
+RGB <- c(207, 90, 0) # paint color -- possible issue here?
+
+## issue with sampleRGB(), doesn't work, gives error message, color intensity too high, any ideas?
+# image <- raster::stack("C:/Users/steve/Documents/eyewearanalysis/images/Edit1_Enhanced/LabA3.png")
+# RGB <- sampleRGB(image, resampleFactor = 1)
 
 ###########
-# WHERE ISSUE ARISES
+# MAJOR ISSUE
 # Returns blank, nothing
 ###########
-target <- imageList['LabA3']
-rasterList_lanRGB <- patLanRGB(imageList, landmarkList, RGB, transformRef = target, resampleFactor = 1, 
+target <- imageList['LabA3'] # doesn't work, returns error message
+rasterList_lanRGB <- patLanRGB(imageList, landmarkList, RGB, transformRef = 'LabA3', resampleFactor = 1, 
                                colOffset = 0.01, crop = TRUE, res = 300, adjustCoords = TRUE, plot = 'stack')
 
