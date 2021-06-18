89432a200b62117e120642e729c3f1bc82d91f16
stevenyuser <77898813+stevenyuser@users.noreply.github.com>
2021-06-18 17:36:42 -0400

    Add files via upload

---
+++ eyewear_script.R
@@ -0,0 +1,32 @@
+# Eyewear particles script - analysis
+
+# load patternize
+library(patternize)
+
+# List with samples
+IDlist <- c('LabA3',
+            'LabB3',
+            'ReadingA2',
+            'ReadingB3')
+
+# landmark list
+prepath <- 'landmarks'
+extension <- '_landmarks.txt'
+landmarkList <- makeList(IDlist, 'landmark', prepath, extension)
+
+# image list
+prepath <- 'images/Edit1_Enhanced'
+extension <- '.png'
+imageList <- makeList(IDlist, 'image', prepath, extension)
+
+# align color patterns
+RGB <- c(207, 90, 0) # red color
+
+###########
+# WHERE ISSUE ARISES
+# Returns blank, nothing
+###########
+target <- imageList['LabA3']
+rasterList_lanRGB <- patLanRGB(imageList, landmarkList, RGB, transformRef = target, resampleFactor = 1, 
+                               colOffset = 0.01, crop = TRUE, res = 300, adjustCoords = TRUE, plot = 'stack')
+
